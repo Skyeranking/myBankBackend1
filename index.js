e7bf1f94e331b8bb76c707c46dac08fb2905fbae
Oluwaseun Ajisefinni <Ajisefinnioluwaseun@gmail.com>
2025-08-20 07:06:28 +0100

    cors

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const express = require('express')
 const app = express()
 const ejs = require('ejs')
 const cors = require('cors')
-app.use(cors({
-    origin: 'http://localhost:5173',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
-}))
+app.use(cors());
 const path = require('path')
 app.use(express.static(path.join(__dirname, 'pubic')))
 app.set('view engine', 'ejs')
